c1816f62b2b05539a9aee839931455358ea6819f
baniles <71946860+baniles@users.noreply.github.com>
2023-03-25 15:28:59 -0500

    decode and execute

--- demo1/verilog/decode.v
+++ demo1/verilog/decode.v
@@ -5,34 +5,37 @@
    Description     : This is the module for the overall decode stage of the processor.
 */
 `default_nettype none
-module decode (read_data1, read_data2, sign_ext_11_16, sign_ext_8_16, sign_ext_5_16, zero_ext_8_16, zero_ext_5_16, pc_inc_out, instr_10_2,
-		pc_inc_in, write_data_in, clk, rst);
+module decode (read_data1, read_data2, se_11_16, se_8_16, se_5_16, ze_8_16, ze_5_16, pc_inc_out,
+		pc_inc_in, write_data_in, clk, rst, err, writeEn, instr);
 
    // TODO: Your code here
    output wire [15:0] read_data1, read_data2;
-   output wire [15:0] sign_ext_11_16;
-   output wire [15:0] sign_ext_8_16;
-   output wire [15:0] sign_ext_5_16;
-   output wire [15:0] zero_ext_8_16;
-   output wire [15:0] zero_ext_5_16;
+   output wire [15:0] se_11_16;
+   output wire [15:0] se_8_16;
+   output wire [15:0] se_5_16;
+   output wire [15:0] ze_8_16;
+   output wire [15:0] ze_5_16;
    output wire [15:0] pc_inc_out;
-   output wire [15:2] instr_10_2;
+   // output wire [15:2] instr_10_2; // Not sure on purpose
+   output wire 	      err;
 
-   input wire clk, rst;
+   input wire clk, rst, RegWrite;
+   input wire [2:0] writeRegSel;
    input wire [15:0] pc_inc_in;
    input wire [15:0] write_data_in;
+   input wire [15:0] instr;
 
+   assign pc_inc_out = pc_inc_in;
 
    // register file (non-bypassing)
-   rf register_file(.read1OutData(read_data1),.read2OutData(read_data2),.err(),.clk(clk),.rst(rst),.read1RegSel(),.read2RegSel(),.writeRegSel(),.writeInData(),.writeEn()); 
-
+   rf register_file(.read1OutData(read_data1),.read2OutData(read_data2),.err(err),.clk(clk),.rst(rst),.read1RegSel(inst[10:8]),.read2RegSel(instr[7:5]),.writeRegSel(writeRegSel),.writeInData(write_data_in),.writeEn(RegWrite)); 
 
    // sign extend modules
-   sign_ext_11_16 (.out(),.in());
-   sign_ext_8_16(.out(),.in());
-   sign_ext_5_16(.out(),.in()); 
-   zero_ext_5_16(.out(),.in());
-   zero_ext_8_16(.out(),.in());
+   sign_ext_11_16 se11_16(.out(se_11_16),.in(instr));
+   sign_ext_8_16 se8_16(.out(se_8_16),.in(instr));
+   sign_ext_5_16 se5_16(.out(se_5_16),.in(instr)); 
+   zero_ext_5_16 ze5_16(.out(ze_5_16),.in(instr));
+   zero_ext_8_16 ze8_16(.out(ze_8_16),.in(instr));
    
 endmodule
 `default_nettype wire
--- demo1/verilog/execute.v
+++ demo1/verilog/execute.v
@@ -5,27 +5,45 @@
    Description     : This is the overall module for the execute stage of the processor.
 */
 `default_nettype none
-module execute (pc_inc_in, pc_new, alu_res, instr_10_2_out, alu_cond,
-		read_data1_in, read_data2_in, sign_ext_11_16, sign_ext_8_16, sign_ext_5_16, zero_ext_5_16, zero_ext_8_16, instr_10_2_in, clk, rst);
+module execute (pc_inc_in, pc_new, alu_res, alu_cond,
+		read_data1_in, read_data2_in, se_11_16, se_8_16, se_5_16, ze_5_16, ze_8_16, clk, rst. PCAddSel, PCImmSel, aluSrc);
 
    // TODO: Your code here
    output wire [15:0] pc_inc_in;
    output wire [15:0] pc_new;
    output wire [15:0] alu_res;
-   output wire [10:2] instr_10_2_out;
+   //output wire [10:2] instr_10_2_out;
    output wire alu_cond;
 
    input wire clk, rst;
    input wire [15:0] read_data1_in, read_data2_in;
-   input wire [15:0] sign_ext_11_16;
-   input wire [15:0] sign_ext_8_16;
-   input wire [15:0] sign_ext_5_16;
-   input wire [15:0] zero_ext_5_16;
-   input wire [15:0] zero_ext_8_16;
-   input wire [10:2] instr_10_2_in;
+   input wire [15:0] se_11_16;
+   input wire [15:0] se_8_16;
+   input wire [15:0] se_5_16;
+   input wire [15:0] ze_5_16;
+   input wire [15:0] ze_8_16;
+   input wire 	     PCAddSel;
+   input wire 	     PCImmSel;
+   input wire [2:0] 	 aluSrc;
+ 
+   //input wire [10:2] instr_10_2_in; // Not sure on purpose
 
+   // Intermediate wires to pass to pc adder
+   wire [15:0] 	     pc_add_A;
+   wire [15:0] 	     pc_add_B;
 
-   alu ex_alu(.InA(),.InB(),.Cin(),.Oper(),.invA(),.invB(),.sign(),.Out(),.Zero(),.Ofl()); 
+   // Intermediate wire for InB of alu
+   wire [15:0] 	     InB;
+
+   // Mux to choose inB for alu
+   assign InB = aluSrc[2] ?  (aluSrc[0] ? ze_8_16 : ze_5_16) : (aluSrc[1] ? (aluSrc[0] ? se_5_16 : 16'b0) : (aluSrc[0] ? read_data2_in : se_8_16));
+
+   // Muxes and adder for PC
+   assign pc_add_A = PCAddSel ? read_data1_in : pc_inc_in;
+   assign pc_add_B = PCImmSel ? se_8_16 : se_11_16;
+   cla16b pc_add(.sum(pc_new), .cOut(), .inA(pc_add_A), .inB(pc_add_B), .cIn(1'b0), .sub(1'b0));
+   
+   alu ex_alu(.InA(read_data1_in), .InB(InB), .Cin(1'b0), .Oper(), .invA(1'b0), .invB(1'b0), .sign(1'b0), .Out(alu_res), .Zero(), .Ofl(), .equal(), .lt(), .lte(), .cOut()); 
    
 endmodule
 `default_nettype wire
