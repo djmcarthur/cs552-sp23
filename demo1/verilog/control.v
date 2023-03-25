3c4465c09528a07bb3e42d31d6bc5d2da059926a
Douglas McArthur <mcarthur@snares-07.cs.wisc.edu>
2023-03-25 12:46:50 -0500

    control file initial commit

---
+++ demo1/verilog/control.v
@@ -0,0 +1,18 @@
+/*
+ *  Control logic
+ */
+`default_nettype none
+module control (ALU_Op, ALU_Src, instr);
+
+    output wire ALU_Op, ALU_Src;
+
+    input [15:0] instr;
+
+
+    always @ (*) begin
+
+    end
+
+
+endmodule
+`default_nettype wire
