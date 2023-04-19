1935fcce2b84abd4d1f00c0b4c629de6ddee3710
baniles <71946860+baniles@users.noreply.github.com>
2023-04-19 14:43:55 -0500

    Add files via upload

--- cache/cache_direct/verilog/mem_system.v
+++ cache/cache_direct/verilog/mem_system.v
@@ -27,42 +27,80 @@ module mem_system(/*AUTOARG*/
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
    parameter memtype = 0;
+
+   /* state machine control vars */
+   wire [4:0] 	     state;
+   reg [4:0] 	     next_state;
+
+   /* state machine signals */
+
+   // going in to mem or cache
+   reg 		     enable, comp, write, valid_in;
+   reg [3:0] 	     offset;
+   reg 		     rd,wr;
+   reg [4:0] 	     mem_tag;
+   reg [2:0] 	     mem_off;
+   reg [15:0] 	     mem_in;
+
+   // coming out out mem or cache
+   wire 	     cache_err,mem_err, hit, dirty, valid;
+   wire [4:0] 	     tag_out;
+   wire [15:0] 	     data_out;
+   wire 	     mem_stall;
+   wire [3:0] 	     busy;
+   wire [15:0] 	     mem_out;
+   
    cache #(0 + memtype) c0(// Outputs
-                          .tag_out              (),
-                          .data_out             (),
-                          .hit                  (),
-                          .dirty                (),
-                          .valid                (),
-                          .err                  (),
+                          .tag_out              (tag_out),
+                          .data_out             (data_out),
+                          .hit                  (hit),
+                          .dirty                (dirty),
+                          .valid                (valid),
+                          .err                  (cache_err),
                           // Inputs
-                          .enable               (),
-                          .clk                  (),
-                          .rst                  (),
-                          .createdump           (),
-                          .tag_in               (),
-                          .index                (),
-                          .offset               (),
-                          .data_in              (),
-                          .comp                 (),
-                          .write                (),
-                          .valid_in             ());
+                          .enable               (enable),
+                          .clk                  (clk),
+                          .rst                  (rst),
+                          .createdump           (createdump),
+                          .tag_in               (Addr[15:11]),
+                          .index                (Addr[10:3]),
+                          .offset               (offset),
+                          .data_in              (DataIn),
+                          .comp                 (comp),
+                          .write                (write),
+                          .valid_in             (valid_in));
 
    four_bank_mem mem(// Outputs
-                     .data_out          (),
-                     .stall             (),
-                     .busy              (),
-                     .err               (),
+                     .data_out          (mem_out),
+                     .stall             (mem_stall),
+                     .busy              (busy),
+                     .err               (mem_err),
                      // Inputs
-                     .clk               (),
-                     .rst               (),
-                     .createdump        (),
-                     .addr              (),
-                     .data_in           (),
-                     .wr                (),
-                     .rd                ());
+                     .clk               (clk),
+                     .rst               (rst),
+                     .createdump        (createdump),
+                     .addr              (mem_addr),
+                     .data_in           (data_out),
+                     .wr                (wr),
+                     .rd                (rd));
    
    // your code here
+   assign mem_addr = {mem_tag, Addr[10:3], mem_offset};
 
+   always @(*) begin
+      err = mem_err | cache_err;
+      enable = 1'b0;
+      offset = Addr[2:0];
+      comp = 1'b0;
+      write = 1'b0;
+      valid_in = 1'b0;
+      wr = 1'b0;
+      rd = 1'b0;
+      Done = 1'b0;
+      Stall = 1'b0;
+      CacheHit = 1'b0;
+      err = mem_err | cache_err;
+      
    
 endmodule // mem_system
 `default_nettype wire
